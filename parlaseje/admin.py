3de14069f459353168b2895d9ff9e2f1a0b15ea9
muki <root@parlameter.st.si>
2015-10-25 15:27:15 +0100

    registered some extra models in the admin

--- parlaseje/admin.py
+++ parlaseje/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from parlaseje.models import Session, Vote_graph
+from parlaseje.models import Session, Vote_graph, Vote
 
 
 # Register your models here.
 admin.site.register(Session)
 admin.site.register(Vote_graph)
+admin.site.register(Vote)
