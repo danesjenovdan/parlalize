69b5cb66caa7641965ddef3f6dc17f098cfed7bf
Tomaž Kunst <tomazkunst@gmail.com>
2016-07-14 16:49:27 +0200

    add getPersonData to utils, fix all getters that return standard personData

--- parlalize/utils.py
+++ parlalize/utils.py
@@ -508,3 +508,15 @@ def getMPGovId(id_parladata):
     out = {"id":person.id_parladata, "gov_id":person.gov_id}
     print out
     return out
+
+
+def getPersonData(id_parladata, date_=None):
+    if not date_:
+        date_ = datetime.now().strftime(API_DATE_FORMAT)
+    data = getPersonCardModelNew(MPStaticPL, id_parladata, date_)
+    return {
+            'name': data.person.name,
+            'id': int(data.person.id_parladata),
+            'gov_id': data.gov_id,
+            'party': Organization.objects.get(id_parladata=data.party_id).getOrganizationData()
+        }
