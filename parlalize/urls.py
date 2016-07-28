1e88b14b7af24fbd156fb75c235d148a879c51c7
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-07-28 16:34:15 +0200

    added getPersonData url API endpoint

--- parlalize/urls.py
+++ parlalize/urls.py
@@ -2,12 +2,15 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
+from utils import getPersonData
+
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^v1/p/', include('parlaposlanci.urls')),
-    (r'^v1/pg/', include('parlaskupine.urls')), 
-    (r'^v1/s/', include('parlaseje.urls'))
+    (r'^v1/pg/', include('parlaskupine.urls')),
+    (r'^v1/s/', include('parlaseje.urls')),
+    (r'^v1/utils/getPersonData/(?P<person_id>\d+)/(?P<date_>[\w].+)', getPersonData),
 
 )
