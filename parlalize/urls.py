b2f57c636ba09e120ed20b58750e318874447b65
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-07-28 16:38:16 +0200

    bug fix

--- parlalize/urls.py
+++ parlalize/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-from utils import getPersonData
+from utils import getPersonDataAPI
 
 # admin.autodiscover()
 
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     (r'^v1/p/', include('parlaposlanci.urls')),
     (r'^v1/pg/', include('parlaskupine.urls')),
     (r'^v1/s/', include('parlaseje.urls')),
-    (r'^v1/utils/getPersonData/(?P<id_parladata>\d+)/(?P<date_>[\w].+)', getPersonData),
-    (r'^v1/utils/getPersonData/(?P<id_parladata>\d+)', getPersonData),
+    (r'^v1/utils/getPersonData/(?P<id_parladata>\d+)/(?P<date_>[\w].+)', getPersonDataAPI),
+    (r'^v1/utils/getPersonData/(?P<id_parladata>\d+)', getPersonDataAPI),
 
 )
