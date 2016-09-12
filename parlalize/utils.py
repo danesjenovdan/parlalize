aaf8079cd960450373d07a29abe18f2098ec3a17
Tomaž Kunst <tomazkunst@gmail.com>
2016-09-12 16:47:45 +0200

    runner for specific day, remove print kwargs in saveorabort

--- parlalize/utils.py
+++ parlalize/utils.py
@@ -144,7 +144,7 @@ def saveOrAbortNew(model, **kwargs):
         return True
     if model != LastActivity:
         created_for = kwargs.pop('created_for')
-    print kwargs
+    #print kwargs
     savedModel = model.objects.filter(**kwargs)
     if savedModel:
         if 'person' in kwargs:
@@ -338,7 +338,7 @@ def updateBallots():
     existingISs = Ballot.objects.all().values_list("id_parladata", flat=True)
     for dic in data:
         if int(dic["id"]) not in existingISs:#Ballot.objects.filter(id_parladata=dic['id']):
-            print "adding ballot"
+            print "adding ballot " + str(dic['vote'])
             vote = Vote.objects.get(id_parladata=dic['vote'])
             ballots = Ballot(person=Person.objects.get(id_parladata=int(dic['voter'])),
                              option=dic['option'],
