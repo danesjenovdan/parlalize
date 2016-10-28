4ad5826490c426ab903523e58aa8216995295a0a
Tomaž Kunst <tomazkunst@gmail.com>
2016-10-28 17:19:03 +0200

    typo

--- parlaseje/views.py
+++ parlaseje/views.py
@@ -473,7 +473,7 @@ def getLastSessionLanding(request, date_=None):
         fdate=datetime.now().today()
     ready = False
     presences = PresenceOfPG.objects.filter(created_for__lte=fdate).order_by("-created_for")
-    if not presence:
+    if not presences:
         raise Http404("Nismo našli kartice")
     presence_intex = 0
     motions = None
