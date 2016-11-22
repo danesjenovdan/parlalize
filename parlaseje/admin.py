7eb04ba68ebe11fa84f066c6b5ee8558aa82f571
T0mTom <juric.tomaz@gmail.com>
2016-11-22 20:22:24 +0100

    added model to admin

--- parlaseje/admin.py
+++ parlaseje/admin.py
@@ -1,8 +1,18 @@
+import sys
 from django.contrib import admin
-from parlaseje.models import Session, Vote_graph, Vote
-
+from parlaseje.models import *
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Register your models here.
 admin.site.register(Session)
 admin.site.register(Vote_graph)
 admin.site.register(Vote)
+admin.site.register(Activity)
+admin.site.register(Speech)
+admin.site.register(Ballot)
+admin.site.register(AbsentMPs)
+admin.site.register(Quote)
+admin.site.register(PresenceOfPG)
+admin.site.register(Tag)
+admin.site.register(AverageSpeeches)
--- parlaskupine/admin.py
+++ parlaskupine/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from parlaskupine.models import *
 # Register your models here.
+
+admin.site.register(Organization)
+admin.site.register(PGStatic)
+admin.site.register(PercentOFAttendedSession)
+admin.site.register(MPOfPg)
+admin.site.register(MostMatchingThem)
+admin.site.register(LessMatchingThem)
+admin.site.register(DeviationInOrganization)
+admin.site.register(CutVotes)
+admin.site.register(WorkingBodies)
+admin.site.register(VocabularySize)
+admin.site.register(StyleScores)
