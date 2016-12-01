693b3c02ead669284b24f7a2a02b195c9dbc41fa
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-12-01 23:01:23 +0100

    added quote_id to getSpeech and getQuote

--- parlaseje/views.py
+++ parlaseje/views.py
@@ -25,7 +25,8 @@ def getSpeech(request, speech_id):
          "session": speech.session.getSessionData(), 
          "quoted_text": None, 
          "end_idx": None, 
-         "start_idx": None}
+         "start_idx": None
+         'quote_id': None}
 
     result = {
         'person': getPersonData(speech.person.id_parladata, speech.session.start_time.strftime(API_DATE_FORMAT)),
@@ -581,7 +582,8 @@ def getQuote(request, quote_id):
                                      "end_idx": quote.last_char, 
                                      "speech_id": quote.speech.id_parladata,
                                      "content": quote.speech.content,
-                                     'session': quote.speech.session.getSessionData()}})
+                                     'session': quote.speech.session.getSessionData()
+                                     'quote_id': quote.id}})
 
 
 def getLastSessionLanding(request, date_=None):
