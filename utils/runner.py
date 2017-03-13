64000dff96aa5e68b2286e82f536a3e3781e189e
Tomaž Kunst <tomazkunst@gmail.com>
2017-03-13 15:11:06 +0100

    refactoring utils(move depricated runners to depricated)

--- utils/runner.py
+++ utils/runner.py
@@ -6,22 +6,23 @@ from parlalize.settings import API_URL, API_DATE_FORMAT, BASE_URL
 from parlalize.utils import getPGIDs, findDatesFromLastCard
 from datetime import datetime, timedelta
 from django.apps import apps
-from parlaposlanci.models import District
 from raven.contrib.django.raven_compat.models import client
 
 
-from parlaposlanci.views import setCutVotes, setStyleScoresALL, setMPStaticPL, setMembershipsOfMember, setLessEqualVoters, setMostEqualVoters, setPercentOFAttendedSession, setLastActivity, setAverageNumberOfSpeechesPerSessionAll, setVocabularySizeAndSpokenWords, setCompass, getListOfMembers, setTFIDF, getSlugs, setListOfMembersTickers, setPresenceThroughTime
-from parlaposlanci.models import Person, StyleScores, CutVotes, VocabularySize, MPStaticPL, MembershipsOfMember, LessEqualVoters, EqualVoters, Presence, AverageNumberOfSpeechesPerSession, VocabularySize, Compass
+from parlaposlanci.views import setMPStaticPL, setMembershipsOfMember, setLastActivity, setAverageNumberOfSpeechesPerSessionAll, setVocabularySizeAndSpokenWords, setCompass, setListOfMembersTickers, setPresenceThroughTime
+from parlaposlanci.models import Person, MPStaticPL, MembershipsOfMember, AverageNumberOfSpeechesPerSession, Compass
 
-from parlaskupine.views import setCutVotes as setCutVotesPG, setDeviationInOrg, setLessMatchingThem, setMostMatchingThem, setPercentOFAttendedSessionPG, setMPsOfPG, setBasicInfOfPG, setWorkingBodies, setVocabularySizeALL, setStyleScoresPGsALL, setTFIDF as setTFIDFpg, getListOfPGs, setPresenceThroughTime as setPresenceThroughTimePG
-from parlaskupine.models import Organization, WorkingBodies, CutVotes as CutVotesPG, DeviationInOrganization, LessMatchingThem, MostMatchingThem, PercentOFAttendedSession, MPOfPg, PGStatic, VocabularySize as VocabularySizePG, StyleScores as StyleScoresPG
+from parlaskupine.views import setMPsOfPG, setBasicInfOfPG, setWorkingBodies, setVocabularySizeALL, getListOfPGs, setPresenceThroughTime as setPresenceThroughTimePG
+from parlaskupine.models import Organization, WorkingBodies, MPOfPg, PGStatic
 
 from parlaseje.models import Session, Vote, Ballot, Speech, Question, Tag, PresenceOfPG, AbsentMPs, AverageSpeeches, Vote_graph
 from parlaseje.views import setPresenceOfPG, setAbsentMPs, setSpeechesOnSession, setMotionOfSessionGraph, getSessionsList, setMotionOfSession
 from parlaseje.utils import idsOfSession, getSesDates
 from utils.recache import updatePagesS
-from utils.imports import update
+from utils.imports import update, updateDistricts
 from multiprocessing import Pool
+
+from .votes import VotesAnalysis
 
 from parlalize.utils import tryHard, datesGenerator, printProgressBar
 import json
@@ -53,153 +54,6 @@ def updateMPStatic():
         lastObject = change
 
 
-def runSettersMP(date_to):
-    thisDay = datetime.strptime(date_to, API_DATE_FORMAT)
-    toDate = (thisDay - timedelta(days=1)).date()
-    setters_models = {
-        # model: setter,
-        CutVotes: setCutVotes,
-        MembershipsOfMember: setMembershipsOfMember,
-        LessEqualVoters: setLessEqualVoters,
-        EqualVoters: setMostEqualVoters,
-        Presence: setPercentOFAttendedSession,
-
-    }
-    memberships = tryHard(API_URL + '/getAllTimeMemberships').json()
-
-    for membership in memberships:
-        if membership['end_time']:
-            end_time = datetime.strptime(
-                membership['end_time'].split('T')[0], '%Y-%m-%d').date()
-            if end_time > toDate:
-                end_time = toDate
-        else:
-            end_time = toDate
-
-        for model, setter in setters_models.items():
-            print setter, date_to
-            if membership['start_time']:
-                print 'START', membership['start_time']
-                start_time = datetime.strptime(
-                    membership['start_time'].split('T')[0], '%Y-%m-%d')
-                dates = findDatesFromLastCard(model,
-                                              membership['id'],
-                                              end_time.strftime(API_DATE_FORMAT),
-                                              start_time.strftime(API_DATE_FORMAT))
-            else:
-                dates = findDatesFromLastCard(
-                    model, membership['id'],
-                    end_time.strftime(API_DATE_FORMAT))
-            for date in dates:
-                print date.strftime('%d.%m.%Y')
-                print str(membership['id']) + '/' + date.strftime('%d.%m.%Y')
-                try:
-                    setter(None,
-                           str(membership['id']),
-                           date.strftime('%d.%m.%Y'))
-                except:
-                    client.captureException()
-        # setLastActivity allways runs without date
-        setLastActivity(request, str(membership['id']))
-
-    # Runner for setters ALL
-    all_in_one_setters_models = {
-        AverageNumberOfSpeechesPerSession: setAverageNumberOfSpeechesPerSessionAll,
-        VocabularySize: setVocabularySizeAndSpokenWords,
-        Compass: setCompass,
-    }
-
-    zero = datetime(day=2, month=8, year=2014).date()
-    for model, setter in all_in_one_setters_models.items():
-        print(toDate - datetime(day=2, month=8, year=2014).date()).days
-        for i in range((toDate - datetime(day=2, month=8, year=2014).date()).days):
-            print(zero + timedelta(days=i)).strftime('%d.%m.%Y')
-            try:
-                setter(None, (zero + timedelta(days=i)).strftime('%d.%m.%Y'))
-            except:
-                client.captureException()
-
-    return JsonResponse({'status': 'all is fine :D'}, safe=False)
-
-
-def runMembersSetterOnDates(setter, stDate, toDate):
-    stDate = datetime.strptime(stDate, API_DATE_FORMAT).date()
-    toDate = datetime.strptime(toDate, API_DATE_FORMAT).date()
-
-    setters_models = {None: setter}
-
-    memberships = tryHard(API_URL + '/getAllTimeMemberships').json()
-
-    pool = Pool(processes=1)
-    pool.map(doMembersRunner, [{'membership': membership,
-                                'stDate': stDate,
-                                'toDate': toDate,
-                                'setters_models': setters_models}
-                               for membership in memberships])
-
-    return 'all is fine :D'
-
-
-def runSetterOnDates(setter, stDate, toDate):
-    stDate = datetime.strptime(stDate, API_DATE_FORMAT).date()
-    toDate = datetime.strptime(toDate, API_DATE_FORMAT).date()
-
-    dates = datesGenerator(stDate, toDate)
-
-    for date in dates:
-        print 'Setting', date, setter
-        setter(None, date.strftime(API_DATE_FORMAT))
-
-    print 'END ', setter
-
-    return 1
-
-
-def runSettersMPSinglePerson(date_to=None):
-    if not date_to:
-        date_to = datetime.today().strftime(API_DATE_FORMAT)
-
-    toDate = datetime.strptime(date_to, API_DATE_FORMAT).date()
-    zero = datetime(day=2, month=8, year=2014).date()
-    memberships = tryHard(API_URL + '/getAllTimeMemberships').json()
-
-    setters_models = {
-        # model: setter,
-        CutVotes: setCutVotes,
-        MembershipsOfMember: setMembershipsOfMember,
-        LessEqualVoters: setLessEqualVoters,
-        EqualVoters: setMostEqualVoters,
-        Presence: setPercentOFAttendedSession,
-    }
-    for membership in memberships:
-        doMembersRunner({'membership': membership,
-                         'toDate': toDate,
-                         'setters_models': setters_models})
-
-    return 1
-
-
-def runSettersMPAllPerson(date_to=None):
-    if not date_to:
-        date_to = datetime.today().strftime(API_DATE_FORMAT)
-
-    toDate = datetime.strptime(date_to, API_DATE_FORMAT).date()
-    zero = datetime(day=2, month=8, year=2014).date()
-    all_in_one_setters_models = {
-        AverageNumberOfSpeechesPerSession: setAverageNumberOfSpeechesPerSessionAll,
-        VocabularySize: setVocabularySizeAndSpokenWords,
-        Compass: setCompass,
-        StyleScores: setStyleScoresALL,
-    }
-    for model, setter in all_in_one_setters_models.items():
-        doAllMembersRunner({'setters': setter,
-                            'model': model,
-                            'toDate': toDate,
-                            'zero': zero})
-
-    return 1
-
-
 def onDateMPCardRunner(date_=None):
     """
     Create all cards for data_ date. If date_ is None set for run setters
@@ -215,13 +69,8 @@ def onDateMPCardRunner(date_=None):
         date_of = (datetime.now() - timedelta(days=1)).date()
         date_ = date_of.strftime(API_DATE_FORMAT)
     setters = [
-        setCutVotes,
         setMembershipsOfMember,
-        setLessEqualVoters,
-        setMostEqualVoters,
-        setPercentOFAttendedSession,
         setPresenceThroughTime,
-        # setTFIDF
     ]
 
     memberships = tryHard(API_URL + '/getMPs/' + date_).json()
@@ -257,105 +106,6 @@ def onDateMPCardRunner(date_=None):
             print 'FAIL on: ' + str(setter)
 
 
-# parlaseje runners methods #
-
-def runSettersPG(date_to=None):
-    if not date_to:
-        date_to = datetime.today().strftime(API_DATE_FORMAT)
-    dateObj = datetime.strptime(date_to, API_DATE_FORMAT)
-    toDate = (dateObj - timedelta(days=1)).date()
-    setters_models = {
-        CutVotesPG: setCutVotesPG,
-        DeviationInOrganization: setDeviationInOrg,
-        LessMatchingThem: setLessMatchingThem,
-        MostMatchingThem: setMostMatchingThem,
-        PercentOFAttendedSession: setPercentOFAttendedSessionPG,
-        MPOfPg: setMPsOfPG,
-        PGStatic: setBasicInfOfPG,
-    }
-
-    IDs = getPGIDs()
-    # IDs = [1, 2]
-    # print IDs
-    allIds = len(IDs)
-    curentId = 0
-    start_time = None
-    end_time = None
-    for model, setter in setters_models.items():
-        for ID in IDs:
-            print setter
-            start_time = None
-            end_time = None
-            url_date = ('/' + date_to if date_to else '/')
-            url = API_URL + '/getMembersOfPGRanges/' + str(ID) + url_date
-            membersOfPGsRanges = tryHard(url).json()
-
-            # find if pg exist
-            for pgRange in membersOfPGsRanges:
-                if not pgRange['members']:
-                    continue
-                else:
-                    if not start_time:
-                        start_time = datetime.strptime(
-                            pgRange['start_date'], '%d.%m.%Y').date()
-
-                    end_time = datetime.strptime(
-                        pgRange['end_date'], '%d.%m.%Y').date()
-
-            if not start_time:
-                continue
-
-            dates = findDatesFromLastCard(model,
-                                          ID,
-                                          end_time.strftime(API_DATE_FORMAT),
-                                          start_time.strftime(API_DATE_FORMAT))
-            print dates
-            for date in dates:
-                if date < start_time or date > end_time:
-                    break
-                print date.strftime(API_DATE_FORMAT)
-                # print setter + str(ID) + '/' + date.strftime(API_DATE_FORMAT)
-                try:
-                    setter(None, str(ID), date.strftime(API_DATE_FORMAT))
-                except:
-                    client.captureException()
-        curentId += 1
-
-    # Runner for setters ALL
-    all_in_one_setters_models = {
-        VocabularySizePG: setVocabularySizeALL,
-        StyleScoresPG: setStyleScoresPGsALL,
-    }
-
-    zero = datetime(day=2, month=8, year=2014).date()
-    for model, setter in all_in_one_setters_models.items():
-        if model.objects.all():
-            zero = model.objects.all().latest('created_for').created_for
-        print(toDate - datetime(day=2, month=8, year=2014).date()).days
-        for i in range((toDate - zero.date()).days):
-            print(zero + timedelta(days=i)).strftime('%d.%m.%Y')
-            print setter
-            try:
-                setter(None, (zero + timedelta(days=i)).strftime('%d.%m.%Y'))
-            except:
-                client.captureException()
-
-    organizations = tryHard(API_URL + '/getOrganizatonByClassification').json()
-    print organizations
-    for org in organizations['working_bodies'] + organizations['council']:
-        print org
-        dates = findDatesFromLastCard(WorkingBodies, org['id'], date_to)
-        for date in dates:
-            try:
-                print setWorkingBodies(None,
-                                       str(org['id']),
-                                       date.strftime(API_DATE_FORMAT)).content
-            except:
-                client.captureException()
-
-    return 'all is fine :D PG ji so settani'
-
-
 def onDatePGCardRunner(date_=None):
     FAIL = '\033[91m'
     ENDC = '\033[0m'
@@ -368,11 +118,6 @@ def onDatePGCardRunner(date_=None):
         date_ = date_of.strftime(API_DATE_FORMAT)
     print date_
     setters = [
-        setCutVotesPG,
-        setDeviationInOrg,
-        setLessMatchingThem,
-        setMostMatchingThem,
-        setPercentOFAttendedSessionPG,
         setMPsOfPG,
         setBasicInfOfPG,
         setPresenceThroughTimePG,
@@ -396,7 +141,7 @@ def onDatePGCardRunner(date_=None):
 
     # Runner for setters ALL
     all_in_one_setters = [
-        # setVocabularySizeALL,
+        setVocabularySizeALL,
     ]
 
     for setter in all_in_one_setters:
@@ -526,13 +271,6 @@ def updateWB():
             client.captureException()
 
     return 'all is fine :D WB so settani'
-
-
-def deleteUnconnectedSpeeches():
-    data = tryHard(API_URL + '/getAllSpeeches').json()
-    idsInData = [speech['id'] for speech in data]
-    blindSpeeches = Speech.objects.all().exclude(id_parladata__in=idsInData)
-    blindSpeeches.delete()
 
 
 def fastUpdate(date_=None):
