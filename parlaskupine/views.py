600d03a3014ab12101d3629a4490e465fed2535d
T0mTom <juric.tomaz@gmail.com>
2017-05-24 21:00:06 +0200

    pag

--- parlaskupine/views.py
+++ parlaskupine/views.py
@@ -1565,16 +1565,15 @@ def getIntraDisunion(request):
     ob =  {}
     obs =  {}
     dataOut =  {}
-    #paginator = Paginator(Vote.objects.all().order_by('start_time'), 50)                                    
-    votespag = Vote.objects.all().order_by('start_time')                                 
-    '''page = request.GET.get('page', 1)
+    paginator = Paginator(Vote.objects.all().order_by('start_time'), 50)                                                          
+    page = request.GET.get('page', 1)
     try:
         votespag = paginator.page(page)
     except PageNotAnInteger:
         votespag = paginator.page(1)
     except EmptyPage:
         votespag = paginator.page(paginator.num_pages)
-    '''
+    
     for vote in votespag:
         votesData[vote.id_parladata] = {'text':vote.motion,
                                         'result':vote.result,
@@ -1617,9 +1616,7 @@ def getIntraDisunionOrg(request, org_id):
     ob = {}
     obj = {}
     ob['votes'] = []
-    #paginator = Paginator(Vote.objects.all().order_by('start_time'), 50)                                    
-    votespag = Vote.objects.all().order_by('start_time')                                
-    '''
+    paginator = Paginator(Vote.objects.all().order_by('start_time'), 50)                                    
     page = request.GET.get('page', 1)
     try:
         votespag = paginator.page(page)
@@ -1627,7 +1624,6 @@ def getIntraDisunionOrg(request, org_id):
         votespag = paginator.page(1)
     except EmptyPage:
         votespag = paginator.page(paginator.num_pages)
-    '''
     for vote in votespag:
         votesData[vote.id_parladata] = {'text':vote.motion,
                                         'result':vote.result,
