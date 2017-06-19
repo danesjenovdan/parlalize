fd6cc77b7c06daf58cdeb284427f46f67f1e15a8
Tomaž Kunst <tomazkunst@gmail.com>
2017-06-19 14:03:40 +0200

    add sentry logging to update from shell

--- scripts/update_from_shell.py
+++ scripts/update_from_shell.py
@@ -1,7 +1,11 @@
 from utils.runner import update, fastUpdate
+from raven.contrib.django.raven_compat.models import client
 
 
 def run(*args):
     # skip recache lastactivity and speeches
-    skipPersonRecache = True if 'fastUpdate' in args else False
-    fastUpdate(fast=skipPersonRecache)
+    try:
+        skipPersonRecache = True if 'fastUpdate' in args else False
+        fastUpdate(fast=skipPersonRecache)
+    except:
+        client.captureException()
