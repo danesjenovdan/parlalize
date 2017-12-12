7ffda5895b293f33cfc7cf4a4f1595a6cb442713
Tomaž <cofek0@gmail.com>
2017-12-12 15:06:02 +0100

    typo

--- parlaseje/views.py
+++ parlaseje/views.py
@@ -11,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from parlalize.utils_ import tryHard, lockSetter, getAllStaticData, getPersonData, saveOrAbortNew, getDataFromPagerApi
 from parlaseje.models import *
-from parlasseje.utils import hasLegislationLink
+from parlaseje.utils import hasLegislationLink
 from parlalize.settings import API_URL, API_DATE_FORMAT, BASE_URL, SETTER_KEY, ISCI_URL, VOTE_CLASSIFICATIONS
 from parlaskupine.models import Organization
 
