9bae3b014a765207e19930dc3dc94398d2c2a33f
Tomaž <cofek0@gmail.com>
2018-02-20 15:07:27 +0100

    Fixed parsing draft legislation
    * fix get date
    * fix if legislation has no name

--- utils/exports.py
+++ utils/exports.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from parlaseje.models import Legislation
+from parlaposlanci.models import *
+from parlalize.utils_ import tryHard, getPersonCardModelNew
 
 from django.conf import settings
 from django.utils.html import strip_tags
@@ -8,6 +10,7 @@ from datetime import datetime
 
 import requests
 import json
+import csv
 
 def exportLegislations():
     i = 0
@@ -77,3 +80,56 @@ def backupNotes():
     f.write(json.dumps(data))
     f.close()
 
+
+
+def listToCSV(data, file_name):
+    with open(file_name, 'wb') as csvfile:
+        csvwriter = csv.writer(csvfile,
+                               delimiter=',',
+                               quotechar='|',
+                               quoting=csv.QUOTE_MINIMAL)
+
+        if type(data[0]) == dict:
+             csvwriter.writerow(data[0].keys())
+        for value in data:
+            if type(value) == list:
+                e_data = value
+            elif type(value) == tuple:
+                e_data = list(value)
+            elif type(value) == dict:
+                e_data = value.values()
+            else:
+                e_data = [value.values]
+            csvwriter.writerow(e_data)
+    return file_name
+
+
+def data_to_csv():
+    models = [(Presence, 'prisotnost na glasovanjih sej DZ'),
+              (NumberOfQuestions, 'st. poslanskih vprasanj in pobud'),
+              (MismatchOfPG, 'neujemanje s poslansko skupino'),
+              (SpokenWords, 'st. izgovorjenih besed'),
+              (AverageNumberOfSpeechesPerSession, 'stevilo govorov na sejo'),
+              (VocabularySize, 'raznolikost besedisca')]
+
+    mps = tryHard(settings.API_URL+'/getMPs/').json()
+
+    data = []
+
+    for mp in mps:
+        tmp = {'member_id': mp['id']}
+        for model, column in models:
+            print mp['id'], model
+            model_data = getPersonCardModelNew(model, mp['id'])
+            if model == Presence:
+                tmp[column] = model_data.person_value_sessions
+                tmp[column] = model_data.person_value_votes
+            if model == NumberOfQuestions or model == SpokenWords or model == VocabularySize:
+                tmp[column] = model_data.score
+            if model == MismatchOfPG:
+                tmp[column] = model_data.data
+            if model == NumberOfSpeechesPerSession:
+                tmp[column] = model_data.person_value
+        data.append(tmp)
+    listToCSV(data, 'toski_export.csv')
+data_to_csv()
