b487e903b94c7ffb1847a727eff8ffa1e3d8ea9e
Tomaž <cofek0@gmail.com>
2018-07-24 19:50:45 +0200

    fixed getIntadisunion with wierd achronym

--- parlaskupine/views.py
+++ parlaskupine/views.py
@@ -3966,7 +3966,7 @@ def getIntraDisunionOrg(request, org_id, force_render=False):
 
     * @apiSuccessExample {json} Example response:
     {  
-   "SMC":[  
+   "results":[  
       {  
          "text":"Dnevni red v celoti",
          "id_parladata":6513,
@@ -4051,10 +4051,10 @@ def getIntraDisunionOrg(request, org_id, force_render=False):
                             'classification': vote.classification,
                             'maximum': vote.intra_disunion,
                             'id_parladata': vote.id_parladata})
-                out['DZ'] = {'organization': 'dz',
+                out['results'] = {'organization': 'dz',
                              'votes': tab}
 
-            out[str(acr)] = sorted(out[str(acr)]['votes'],
+            out['results'] = sorted(out['results']['votes'],
                                    key=lambda k: k['maximum'])
             out['all_tags'] = list(Tag.objects.all().values_list('name',
                                                                  flat=True))
@@ -4069,9 +4069,9 @@ def getIntraDisunionOrg(request, org_id, force_render=False):
                     obj['maximum'] = float(intra.maximum)
                     ob['votes'].append(obj)
                     ob['organization'] = org.getOrganizationData()
-                out[Organization.objects.get(id_parladata=org_id).acronym] = ob
+                out['results'] = ob
 
-            out[str(acr)] = sorted(out[str(acr)]['votes'],
+            out['results'] = sorted(out[str(acr)]['votes'],
                                    key=lambda k: k['maximum'])
             out['all_tags'] = list(Tag.objects.all().values_list('name',
                                                                  flat=True))
