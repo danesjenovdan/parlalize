fda154f2e9c67325675830df02b84089581c5893
Tomaž <cofek0@gmail.com>
2018-08-20 11:38:08 +0200

    add the order

--- parlaseje/views.py
+++ parlaseje/views.py
@@ -350,6 +350,7 @@ def getSpeechesOfSession(request, session_id):
         out = {"speech_id": speech.id_parladata,
                "content": speech.content,
                "session": sessionData,
+               "the_order": speech.the_order,
                "quoted_text": None,
                "end_idx": None,
                "start_idx": None,
