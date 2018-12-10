8f64496e94c8a6b3ab772b58538e8dc3891bf3d0
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-12-10 16:55:44 +0100

    add setDeviation command

---
+++ parlaposlanci/management/commands/setDeviation.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime
+from parlalize.settings import API_DATE_FORMAT
+from utils.votes_pg import set_mismatch_of_pg
+
+class Command(BaseCommand):
+    help = 'Updates deviation'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--date',
+            nargs=1,
+            help='Date for which to run the card',
+        )
+
+    def handle(self, *args, **options):
+        # TODO refactor votes_pg.py to stop accepting request as an argument
+        set_mismatch_of_pg(None, options['date'])
+
+        return 0
