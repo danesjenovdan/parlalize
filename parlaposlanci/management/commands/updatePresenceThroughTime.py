3e9f8c64c927867d53c15e906b3058bea4a6dc73
Tomaž <cofek0@gmail.com>
2018-12-12 11:00:48 +0100

    add presence an num of questions

---
+++ parlaposlanci/management/commands/updatePresenceThroughTime.py
@@ -0,0 +1,65 @@
+from django.core.management.base import BaseCommand, CommandError
+from parlalize.utils_ import tryHard
+from parlaposlanci.models import Person, PresenceThroughTime
+from parlalize.utils_ import saveOrAbortNew
+from datetime import datetime
+from parlalize.settings import API_URL, API_DATE_FORMAT, YES, NOT_PRESENT, AGAINST, ABSTAIN
+
+
+def setPresenceThroughTime(commander, person_id, date_=None):
+    if date_:
+        fdate = datetime.strptime(date_, '%d.%m.%Y').date()
+    else:
+        fdate = datetime.now().date()
+
+    url = API_URL + '/getBallotsCounterOfPerson/' + person_id + '/' + fdate.strftime(API_DATE_FORMAT)
+    data = tryHard(url).json()
+
+    data_for_save = []
+
+    for month in data:
+        options = YES + NOT_PRESENT + AGAINST + ABSTAIN
+        stats = sum([month[option] for option in options if option in month.keys()])
+        not_member = month['total'] - stats
+        not_member = float(not_member) / month['total'] if not_member else 0
+        presence = float(stats-sum([month[option] for option in NOT_PRESENT  if option in month.keys()])) / month['total'] if stats else 0
+        data_for_save.append({'date_ts': month['date_ts'],
+                              'presence': presence * 100,
+                              'not_member': not_member * 100,
+                              'vote_count': month['total']})
+
+    saveOrAbortNew(model=PresenceThroughTime,
+                   person=Person.objects.get(id_parladata=person_id),
+                   created_for=fdate,
+                   data=data_for_save)
+
+    commander.stdout.write('Set PresenceThroughTime for person id %s' % str(person_id))
+
+
+class Command(BaseCommand):
+    help = 'Updates PresenceThroughTime'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--date',
+            nargs=1,
+            help='date',
+        )
+
+    def handle(self, *args, **options):
+        if options['date']:
+            date_of = datetime.strptime(options['date'], API_DATE_FORMAT).date()
+            date_ = options['date']
+        else:
+            # dirty work around, TODO: fix findDatesFromLastCard for input without person_id
+            #date_of = findDatesFromLastCard(Presence, '11', datetime.now().strftime(API_DATE_FORMAT))[0]
+            date_of = datetime.now().date()
+            date_ = date_of.strftime(API_DATE_FORMAT)
+
+        self.stdout.write('Trying hard for %s/getMPs/%s' % (API_URL, str(date_)))
+        mps = tryHard(API_URL+'/getMPs/' + date_).json()
+        for mp in mps:
+            self.stdout.write('Running setPresenceThroughTime on %s' % str(mp['id']))
+            setPresenceThroughTime(self, str(mp['id']), date_)
+
+        return 0
---
+++ parlaskupine/management/commands/setPGNumOfQuestions.py
@@ -0,0 +1,97 @@
+import json
+
+from collections import Counter
+
+from django.core.management.base import BaseCommand, CommandError
+from parlaposlanci.models import Person
+from parlaskupine.models import Organization, NumberOfQuestions
+from parlalize.utils_ import tryHard, saveOrAbortNew, getDataFromPagerApi, getPersonData
+from datetime import datetime
+from parlalize.settings import API_DATE_FORMAT, API_URL
+
+
+class Command(BaseCommand):
+    help = 'Updates compas'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--date',
+            nargs=1,
+            help='Date for which to run the card',
+        )
+
+        parser.add_argument(
+            '--pgs',
+            nargs='+',
+            help='PG parladata_id',
+            type=int,
+        )
+
+    def handle(self, *args, **options):
+        if options['pgs']:
+            pgs = options['pgs']
+        else:
+            if options['date']:
+                date_ = options['date']
+                date_of = datetime.strptime(date_, API_DATE_FORMAT).date()
+            else:
+                date_ = datetime.now().date().strftime(API_DATE_FORMAT)
+                date_of = datetime.now().date()
+
+            self.stdout.write('Trying hard for %s/getAllQuestions/%s' % (API_URL, date_))
+            url = API_URL + '/getAllQuestions/' + date_
+            data = getDataFromPagerApi(url)
+            self.stdout.write('Trying hard for %s/getAllPGs/%s' % (API_URL, date_))
+            url_pgs = API_URL + '/getAllPGs/' + date_
+            pgs_on_date = tryHard(url_pgs).json()
+            self.stdout.write('Trying hard for %s/getMPs/%s' % (API_URL, date_))
+            url = API_URL + '/getMPs/' + date_
+            mps = tryHard(url).json()
+
+            mpStatic = {}
+            for mp in mps:
+                mpStatic[str(mp['id'])] = getPersonData(str(mp['id']), date_)
+
+            self.stdout.write('Trying hard for %s/getAllPGsExt/%s' % (API_URL, date_))
+            allPGs = tryHard(API_URL+'/getAllPGsExt/').json().keys()
+
+            pg_ids = [int(pg_id) for pg_id in pgs_on_date.keys()]
+            authors = []
+            for question in data:
+                qDate = datetime.strptime(question['date'], '%Y-%m-%dT%X')
+                qDate = qDate.strftime(API_DATE_FORMAT)
+                for author in question['author_id']:
+                    try:
+                        person_data = mpStatic[str(author)]
+                    except KeyError as e:
+                        print(str(question['author_id']))
+                        person_data = getPersonData(str(author), date_)
+                        mpStatic[str(author)] = person_data
+                    if person_data and person_data['party'] and person_data['party']['id']:
+                        authors.append(person_data['party']['id'])
+                    else:
+                        print 'person nima mpstatic: ', author
+
+            avg = len(authors)/float(len(pg_ids))
+            question_count = Counter(authors)
+            max_value = 0
+            max_orgs = []
+            for maxi in question_count.most_common(90):
+                if max_value == 0:
+                    max_value = maxi[1]
+                if maxi[1] == max_value:
+                    max_orgs.append(maxi[0])
+                else:
+                    break
+            is_saved = []
+            for pg_id in pg_ids:
+                org = Organization.objects.get(id_parladata=pg_id)
+                is_saved.append(saveOrAbortNew(model=NumberOfQuestions,
+                                            created_for=date_of,
+                                            organization=org,
+                                            score=question_count[pg_id],
+                                            average=avg,
+                                            maximum=max_value,
+                                            maxOrgs=max_orgs))
+
+        return 0
