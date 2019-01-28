563a5bb7c97552f1c0a7894fd9cd29afa21d7b76
Tomaž <cofek0@gmail.com>
2019-01-28 12:52:07 +0100

    * added epa filter to laws api * fixed duplicated update legislations

--- parlaseje/management/commands/updateLegislation.py
+++ parlaseje/management/commands/updateLegislation.py
@@ -27,77 +27,61 @@ class Command(BaseCommand):
         self.stdout.write('Getting data from %s/law/' % API_URL)
         laws = getDataFromPagerApiDRF(API_URL + '/law/')
         epas = list(set([law['epa'] for law in laws if law['epa']]))
-        hr_acts = [law for law in laws if not law['epa']]
         self.stdout.write('Iterating through EPAs')
         for epa in set(epas):
             self.stdout.write(str(epa))
             laws = requests.get(API_URL + '/law?epa=' + str(epa),
                 auth=requests.auth.HTTPBasicAuth(PARSER_UN, PARSER_PASS)).json()
-            # self.stdout.write(laws)
-            if int(laws['count']) > 1:
-                sorted_date = sorted(laws['results'], key=lambda x: datetime.strptime(x['date'].split('T')[0], '%Y-%m-%d'))
-                # self.stdout.write(str(sorted_date))
-                sessions = list(set(list([Session.objects.get(id_parladata=int(l['session']))
-                            for l 
-                            in sorted_date
-                            if l['session']])))
-                sorted_date = sorted_date[0]
-                self.stdout.write('Setting legislation %s' % str(sorted_date['epa']))
-                result = Legislation(text=sorted_date['text'],
-                                    epa=sorted_date['epa'],
-                                    mdt=sorted_date['mdt'],
-                                    proposer_text=sorted_date['proposer_text'] if sorted_date['proposer_text'] else None,
-                                    procedure_phase=sorted_date['procedure_phase'],
-                                    procedure=sorted_date['procedure'],
-                                    type_of_law=sorted_date['type_of_law'],
-                                    classification=sorted_date['classification'],
-                                    date=sorted_date['date'],
-                                    status=sorted_date['status'],
-                                    #mdt_fk=sorted_date['mdt_fk']
-                                    )
-                if sorted_date['result']:
-                    result.result = sorted_date['result']
-                result.save()
-                print(sessions)
-                if sessions:
-                    result.sessions.add(*sessions)
-            else:
-                self.stdout.write('Setting legislation %s' % str(laws['results'][0]['epa']))
-                result = Legislation(text=laws['results'][0]['text'],
-                                    epa=laws['results'][0]['epa'],
-                                    mdt=laws['results'][0]['mdt'],
-                                    proposer_text=laws['results'][0]['proposer_text'],
-                                    procedure_phase=laws['results'][0]['procedure_phase'],
-                                    procedure=laws['results'][0]['procedure'],
-                                    type_of_law=laws['results'][0]['type_of_law'],
-                                    classification=laws['results'][0]['classification'],
-                                    status=laws['results'][0]['status'],
-                                    date=laws['results'][0]['date'],
-                                    #mdt_fk=laws['results']['mdt_fk']
-                                    )
-                result.save()
+            last_obj = None
+            sessions = []
+            is_ended = False
+            for law in laws:
+                sessions.append(law['session'])
+                law['date'] = datetime.strptime(law['date'], '%Y-%m-%dT%X')
+                if not is_ended:
+                    if law['procedure_ended']:
+                        is_ended = True
+                if last_obj:
+                    if law['date'] > last_obj['date']:
+                        last_obj = law
+                else: 
+                    last_obj = law
+            result = Legislation.objects.filter(epa=epa)
 
-                if law['session']:
-                    # self.stdout.write((law['session']))
-                    result.sessions.add(Session.objects.get(id_parladata=int(law['session'])))
-                if laws['results'][0]['result']:
-                    result.result = laws['results'][0]['result']
+            # dont update Legislatin procedure_ended back to False
+            if result:
+                result = result[0]
+                if result.procedure_ended:
+                    is_ended = True
+                print 'update'
+                result.text = last_obj['text']
+                result.mdt = last_obj['mdt']
+                result.proposer_text = last_obj['proposer_text']
+                result.procedure_phase = last_obj['procedure_phase']
+                result.procedure = last_obj['procedure']
+                result.type_of_law = last_obj['type_of_law']
+                result.id_parladata = last_obj['id']
+                result.date = last_obj['date']
+                result.procedure_ended = is_ended
+                result.classification = last_obj['classification']
                 result.save()
-        for act in hr_acts:
-            result = Legislation(text=act['text'],
-                                epa='akt-'+act['uid'],
-                                mdt=act['mdt'][:255]  if sorted_date['mdt'] else None,
-                                proposer_text=act['proposer_text'][:255]  if sorted_date['proposer_text'] else None,
-                                procedure_phase=act['procedure_phase'],
-                                procedure=act['procedure'],
-                                type_of_law=act['type_of_law'],
-                                classification='akt',
-                                status=act['status'],
-                                #mdt_fk=act['results']['mdt_fk']
-                                )
-            result.save()
-            if act['session']:
-                result.sessions.add(Session.objects.get(id_parladata=int(law['session'])))
-            if act['result']:
-                result.result = act['result']
-            result.save()
+            else:
+                print 'adding'
+                result = Legislation(text=last_obj['text'],
+                                    epa=last_obj['epa'],
+                                    mdt=last_obj['mdt'],
+                                    proposer_text=last_obj['proposer_text'],
+                                    procedure_phase=last_obj['procedure_phase'],
+                                    procedure=last_obj['procedure'],
+                                    type_of_law=last_obj['type_of_law'],
+                                    id_parladata=last_obj['id'],
+                                    date=last_obj['date'],
+                                    procedure_ended=is_ended,
+                                    classification=['classification'],
+                                    )
+                result.save()
+            sessions = list(set(sessions))
+            sessions = list(Session.objects.filter(id_parladata__in=sessions))
+            result.sessions.add(*sessions)
+        return 0
+
