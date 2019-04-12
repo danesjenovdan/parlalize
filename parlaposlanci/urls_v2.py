f7c79033e01c8ebfdf77860d3d689d7cfbabf6dc
Tomaž <cofek0@gmail.com>
2019-04-12 11:36:04 +0200

    rename list of members endpoint

--- parlaposlanci/urls_v2.py
+++ parlaposlanci/urls_v2.py
@@ -4,8 +4,8 @@ from parlaposlanci.views import *
 
 urlpatterns = [
 
-    url(r'^getListOfMembersTickers/(?P<org_id>\d+)/(?P<date_>[\w].+)', getListOfMembersTickers),
-    url(r'^getListOfMembersTickers/(?P<org_id>\d+)', getListOfMembersTickers),
+    url(r'^getListOfMember/(?P<org_id>\d+)/(?P<date_>[\w].+)', getListOfMembersTickers),
+    url(r'^getListOfMembers/(?P<org_id>\d+)', getListOfMembersTickers),
 
     url(r'^getCompass/(?P<org_id>\d+)/(?P<date_>[\w].+)', getCompass),
     url(r'^getCompass/(?P<org_id>\d+)', getCompass),
